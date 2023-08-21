4d3ae587bcf2c62ba7135eea0a9e23f6509e32d4
MiiZZo <miizzo.js@mail.ru>
2023-08-21 03:37:50 +0300

    feat(web/pages/_app): added notifications and animations

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,16 +1,26 @@
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import Link from 'next/link';
+import { StrictMode } from 'react';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-      <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <StrictMode>
+      <MantineProvider>
+        <Head>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="shortcut icon" href="/favicon.svg" />
+        </Head>
+        <Notifications position="top-center" />
+        <AnimatePresence mode="wait" initial={false}>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </MantineProvider>
+    </StrictMode>
   );
 }
