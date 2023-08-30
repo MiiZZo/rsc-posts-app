f33dce5e5968ccf8772080b9b50e165d0ab06160
MiiZZo <miizzo.js@mail.ru>
2023-08-30 06:50:25 +0300

    refactor(web/pages/posts): added formatting createdAt post date via postsLib.formatPostCreateDate

--- packages/web/src/pages/posts/[id].tsx
+++ packages/web/src/pages/posts/[id].tsx
@@ -2,35 +2,23 @@ import { useUnit } from 'effector-react';
 import { Title, Text, Group, Anchor } from '@mantine/core';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useMemo } from 'react';
 import ReactMarkdown from 'react-markdown'
 import { GSSFactory } from 'shared/nextjs';
 import { paths } from 'shared/navigation';
 import { ClientOnly } from 'shared/ui/client-only';
 import { Avatar } from 'shared/ui/avatar';
-import { dayjs } from 'shared/dates';
 import * as model from './model';
 
 export default function PostPage() {
-  const post = useUnit(model.$post)!;
+  const post = useUnit(model.$formattedPost)!;
   const title = `Post it! | ${post.title}`;
-  const createdAt = useMemo(() => {
-    const date = dayjs(post.createdAt);
-    const diffInMonths = dayjs().diff(date, 'weeks');
-
-    if (diffInMonths > 2) {
-      return date.format('DD/MM/YYYY');
-    }
-
-    return date.fromNow();
-  }, [post.createdAt]);
 
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <Text c="dimmed">{createdAt}</Text>
+      <Text c="dimmed">{post.createdAt}</Text>
       <Title mt="md">{post.title}</Title>
       <Text c="dimmed" mt="xl" mb="xs">
         Author
--- packages/web/src/pages/posts/model/index.ts
+++ packages/web/src/pages/posts/model/index.ts
@@ -1,8 +1,19 @@
 import { Post } from 'common/types';
 import { createEvent, createStore, sample } from 'effector';
+import { postsLib } from 'entities/posts';
 import { api } from 'shared/api';
 
 export const $post = createStore<Post | null>(null);
+export const $formattedPost = $post.map(
+  (post) => (
+    post ?
+      ({
+        ...post,
+        createdAt: postsLib.formatPostCreateDate(post.createdAt)
+      }) :
+      null
+  )
+);
 
 export const { getOneQuery } = api.posts;
 export const pageOpened = createEvent<{ id: string }>();
