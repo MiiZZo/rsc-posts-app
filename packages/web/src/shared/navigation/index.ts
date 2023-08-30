38d7acc56ee09b18535236823b6ca18f715c9c63
MiiZZo <miizzo.js@mail.ru>
2023-08-30 03:18:18 +0300

    feat(web/shared/navigation): added new paths, remove unused PATHS_CONFIGS object

--- packages/web/src/shared/navigation/index.ts
+++ packages/web/src/shared/navigation/index.ts
@@ -1,24 +1,10 @@
-export const PATHS_CONFIG = {
-  '/': {
-    protected: true,
-  },
-  '/posts/:id': {
-    protected: true,
-  },
-  '/sign-in': {
-    protected: false,
-  },
-  '/sign-up': {
-    protected: false,
-  },
-};
-
-export const PROTECTED_PATHS = Object.keys(PATHS_CONFIG).filter((path) => PATHS_CONFIG[path as keyof typeof PATHS_CONFIG].protected);
-
 export const paths = {
   home: () => '/',
   signIn: () => '/sign-in',
   signUp: () => '/sign-up',
+  posts: ({ id }: { id: string }) => `/posts/${id}`,
+  profiles: ({ username }: { username: string }) => `/profiles/${username}`,
+  feed: ({ page }: { page: number }) => `/feed/${page}`,
 };
 
 export const defaultUnprotectedRoute = paths.signIn;
