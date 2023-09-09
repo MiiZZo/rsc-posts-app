4d3e8ecc6f1ff0a4a873256da9f707002c1ef513
MiiZZo <miizzo.js@mail.ru>
2023-09-10 00:01:35 +0300

    feat(web/shared/router): create $query store and RouterEmitter to sync nextjs router with effector

--- packages/web/src/shared/router/index.ts
+++ packages/web/src/shared/router/index.ts
@@ -1,8 +1,31 @@
-import { createEffect } from 'effector';
-import Router from 'next/router';
+import { createEffect, createEvent, createStore, sample } from 'effector';
+import { useUnit } from 'effector-react';
+import Router, { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
+import { useEffect } from 'react';
 
-const navigateFx = createEffect((url: string) => Router.push(url))
+const navigateFx = createEffect((url: string) => Router.push(url));
+const queryChanged = createEvent<ParsedUrlQuery>();
+const $query = createStore<ParsedUrlQuery>({});
+
+sample({
+  clock: queryChanged,
+  target: $query,
+});
 
 export const routerModel ={
   navigateFx,
+  $query,
 };
+
+export function RouterEmitter() {
+  const router = useRouter();
+  const onChangeQuery = useUnit(queryChanged);
+  
+  useEffect(() => {
+    onChangeQuery(router.query);
+  }, [router.query]);
+
+
+  return null;
+}
